980d7b9dd3aa88067a58b3f15db5c6461c613bcc
Hardian-Developer <64962207+Hardian-Developer@users.noreply.github.com>
2020-06-07 10:12:03 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".chat-foto")[0].src = "Foto/Kikiaprilia.jpg";
+document.querySelectorAll(".chat-foto")[1].src = "Foto/Aullea.jpg";
+document.querySelectorAll(".chat-foto")[2].src = "Foto/Virania.jpg";
+document.querySelectorAll(".chat-foto")[3].src = "Foto/Icha.jpg";
+document.querySelectorAll(".chat-foto")[4].src = "Foto/Anggi.jpg";
+document.querySelectorAll(".chat-foto")[5].src = "Foto/Mustika.jpg";
+
+
+document.querySelectorAll(".chat-nama")[0].innerHTML = "Kiki Aprillia";
+document.querySelectorAll(".chat-nama")[1].innerHTML = "Aul Lea";
+document.querySelectorAll(".chat-nama")[2].innerHTML = "Virania";
+document.querySelectorAll(".chat-nama")[3].innerHTML = "Icha";
+document.querySelectorAll(".chat-nama")[4].innerHTML = "Anggi Rizky Nauly Harahap";
+document.querySelectorAll(".chat-nama")[5].innerHTML = "Mustika";
+
+
+document.querySelectorAll(".chat-isi")[0].innerHTML = "Hai, Namaku Kiki Aprilia";
+document.querySelectorAll(".chat-isi")[1].innerHTML = "Hai, Namaku Aul Lea";
+document.querySelectorAll(".chat-isi")[2].innerHTML = "Hai, Namaku Virania";
+document.querySelectorAll(".chat-isi")[3].innerHTML = "Hai, Namaku Icha";
+document.querySelectorAll(".chat-isi")[4].innerHTML = "Hai, Namaku Anggi";
+document.querySelectorAll(".chat-isi")[5].innerHTML = "Hai, Namaku Mustika";
